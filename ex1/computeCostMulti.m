049b301555cce4691bbd48e16cfe14cf0883c6e4
asad98211 <asad98211@gmail.com>
2017-12-31 00:57:54 +0600

    Completed Cost Function for multiple feature

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -7,7 +7,8 @@ function J = computeCostMulti(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+a= sum([[X*theta]-y].^2)/2;
+J = a/m;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
