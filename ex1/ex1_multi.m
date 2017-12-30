c0b39520e3f91e2e9a741ddc325dd060b64c4945
asad98211 <asad98211@gmail.com>
2017-12-31 01:25:20 +0600

    Assignent complete. all ok

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -104,8 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+pred1=featureNormalize([1650,3]);
+pred1=[1 pred1];
+price = pred1*theta; % You should change this
 
 % ============================================================
 
@@ -149,7 +150,9 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+pred1=featureNormalize([1650,3]);
+pred1=[1 pred1];
+price = pred1*theta; % You should change this
 
 
 % ============================================================
